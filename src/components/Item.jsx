0f102a6c7a60fc12194fffaa1cd1312022bc1067
Franco <franco18gonza@gmail.com>
2025-11-12 20:30:02 -0500

    Adjusted the images on the page

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Item({item}){
   return (
     <div style={{border:'1px solid #ddd',padding:10,borderRadius:6}}>
-      <img src={item.img} alt={item.title} style={{width:'100%',height:120,objectFit:'cover'}} />
+      <img src={item.img} alt={item.title} style={{width:'100%',height:120,objectFit:'fill'}} />
       <h3>{item.title}</h3>
       <p>{item.description}</p>
       <p><strong>${item.price}</strong></p>
